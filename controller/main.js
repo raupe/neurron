61dcb42bce17b6fae06873c17d0db73ce163554c
Autarc <Autarc@gmail.com>
2012-10-11 14:50:02 +0200

    add modules

--- controller/main.js
+++ controller/main.js
@@ -1,11 +1,28 @@
 (function(){
 
-	var action = new controller.Action({
+	// reference for display
+	var channel = window.location.hash.substr(1),
 
-		url: 'http://neurron.dev/controller/'
-	});
 
-	action.send( 1212 );
+		// showing element
+		screen = new controller.Screen({
+
+			width: config.canvas.width,
+			height: config.canvas.height
+		}),
+
+		// handling input
+		input = new controller.Input({
+
+			canvas: screen.cvs
+		}),
+
+		// delegating commands
+		action = new controller.Action({
+
+			url: 'http://neurron.dev/controller/',
+			channel: channel
+		});
 
 })();
 
