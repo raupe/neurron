5b48c4d7133912a7f730507d867462b77679067a
ducci <ducci@ducci-U30Sd.(none)>
2012-10-12 16:31:00 +0200

    create main.js for display

---
+++ display/main.js
@@ -0,0 +1,14 @@
+(function(){
+	
+	var action = new display.Action({
+		
+	});
+	
+	var connection = new display.Connection({
+		
+		action: action,
+		server: config.server,
+		port: config.port 
+	});
+	
+})();
