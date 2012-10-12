c9f00d3360880c0db4b8f95eaae52ce6dd39b64e
ducci <ducci@ducci-U30Sd.(none)>
2012-10-12 17:51:09 +0200

    add screen to main

--- display/main.js
+++ display/main.js
@@ -3,8 +3,15 @@
 	display.players = {}; // pool of players
 	display.obstacles = {}; // pool of obstacles
 	
+	var screen = new display.Screen({
+		
+		width: config.canvas.width,
+		height: config.canvas.height		
+	});
+	
 	var action = new display.Action({
 		
+		screen: screen
 	});
 	
 	var connection = new display.Connection({
