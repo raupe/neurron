2ce923da7a68412a7b5258288a3edec451126093
ducci <ducci@ducci-U30Sd.(none)>
2012-10-12 16:33:14 +0200

    create action.js

---
+++ display/scripts/action.js
@@ -0,0 +1,33 @@
+(function(){
+	
+	var Action = display.Action = function(){
+		
+		
+	};
+	
+	Action.prototype.handle = function( data ){
+		
+		var commands = {
+			
+			// those functions are not declared in this scope -> therefore "this" is a reference to prototype
+			1: this.create,
+			2: this.move
+		};
+		
+		commands[data[0]](data[1]); // data[0] can be eg.: 1 for create
+		
+	};
+	
+	Action.prototype.create = function ( options ){
+		
+		console.log("create" + options);
+		
+	};
+	
+	Action.prototype.move = function ( options ){
+		
+		console.log(options);
+	};
+	
+	
+})();
