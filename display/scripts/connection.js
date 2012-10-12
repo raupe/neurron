16fdec10da39c8b2cb6c1037cab5b3eee98826bd
ducci <ducci@ducci-U30Sd.(none)>
2012-10-12 16:33:24 +0200

    create connection.js

---
+++ display/scripts/connection.js
@@ -0,0 +1,37 @@
+(function(){
+	
+	var Connection = display.Connection = function( config ){
+		
+		this.action = config.action;
+		this.url = "ws://" + config.server + ":" + config.port;
+		
+		this.initializeSocket();
+	};
+	
+	Connection.prototype.initializeSocket = function(){
+		
+		// this.socket = new WebSocket( this.url );
+		var action = this.action;
+		
+		// this.socket.onopen = function( message ){
+// 			
+		// };
+// 		
+		// this.socket.onclose = function( message ){
+// 			
+		// };
+		
+		// this.socket.onmessage = function( message ){
+			
+			// message need to be shifted before passing to action
+			var data = [1, 1337];
+			
+			action.handle( data );
+		// };
+		
+		// this.socket.onerror = function( message ){
+// 			
+		// };
+	};
+	
+})();
