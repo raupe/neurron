55d4856fe2bfbedc95977007ee5d7d4216d10145
Autarc <Autarc@gmail.com>
2012-10-18 15:06:10 +0200

    create grid

--- display/scripts/screen.js
+++ display/scripts/screen.js
@@ -1,11 +1,28 @@
 (function(){
 
+
 	var Screen = display.Screen = function ( config ) {
 
 		this.width = config.width;
 		this.height = config.height;
 
 		this.createCanvas();
+		this.createGrid();
+
+		// this.render();
+	};
+
+
+	Screen.prototype.createGrid = function(){
+
+		this.grid = new display.Grid({
+
+			ctx: this.ctx,
+			players: 6,
+			distanceToUser: 350,
+			circleOffset: 100,
+			circles: 7
+		});
 	};
 
 
@@ -27,8 +44,8 @@
 
 	Screen.prototype.scale = function() {
 
-		this.cvs.width = window.innerWidth - window.innerWidth/100;
-		this.cvs.height = window.innerHeight - window.innerHeight/100;
+		this.cvs.width = window.innerWidth - window.innerWidth/20;
+		this.cvs.height = window.innerHeight - window.innerHeight/20;
 	};
 
 
