d0614881383d77e03510bd5af03ad85e4596f2aa
Autarc <Autarc@gmail.com>
2012-10-24 20:49:50 +0200

    fix level access for moving

--- display/scripts/element.js
+++ display/scripts/element.js
@@ -4,16 +4,15 @@
 
         register: function(){
 
-            this.pool[this.id] = this; // add themselve to pool of players
+            this.pool[this.id] = this; // add themselve to pool
 
-            this.nextPos = this.pos = 20;
+            this.nextPos = this.pos = 20;//20;
+            this.moving = false;
 
             this.counter = 0;
 
             this.screen.elements.push( this );
 
-            // console.log(this.pos.x, this.pos.y);
-            // console.log(this.pool);
             this.size = 10;
             this.color = 'blue';
         },
@@ -26,10 +25,10 @@
 
             ctx.fillStyle = this.color;
 
-            if ( this.nextPos === this.pos ) {
-                // console.log(this.nextPos, this.pos);
 
-                ctx.fillRect( field.x - this.size/2, field.y - this.size/2, this.size, this.size);
+            if ( this.nextPos === this.pos ) {
+
+                ctx.fillRect( field.x - this.size/2, field.y - this.size/2, this.size, this.size );
 
             } else {
 
@@ -37,45 +36,70 @@
             }
         },
 
-        move: function ( direction, change ) {
+        move: function ( direction ) {
 
-            if ( direction === 'ctrl' ) {
+            // 4: left, 5: top, 3: right, 6: down
+            var change = ( direction === 4 || direction === 5 ) ? 1 : -1;
 
-                this.dir = ( change > 0 ) ? 'antiRing' : 'ring';
 
-                if ( (this.pos+change) % (this.grid.lanes-1) === 0 ) {
-                    console.log(this.pos);
-                    // change = ( change > 0 ) ?   -(change + this.grid.lanes) :
-                                                // change - this.grid.lanes;
+            // left - right
+            if ( direction === 3 || direction === 4 ) {
+
+                this.dir = ( direction === 4 ) ? 'antiRing' : 'ring';
+
+
+                if ( direction === 4 ) { // edge case: 39 -> 20
+
+                    if ( ( this.pos+change ) % this.grid.lanes === 0 ) {
+
+                        change -= this.grid.lanes * ( ( this.pos+change + 1 ) % this.grid.lanes );
+                    }
+                }
+
+
+                if ( direction === 3 ) { // edge case: 20 -> 39
+
+                    if ( (this.pos+change+1) % this.grid.lanes === 0 ) {
+
+                        change += this.grid.lanes;
+                    }
                 }
             }
 
-            if ( direction === 'shift' ) {
 
-                this.dir = ( change > 0 ) ? 'dist' : 'antiDist';
-                change = ( change > 0 ) ?   change + this.grid.lanes - 1 :
-                                            change - this.grid.lanes + 1;
+            // up - down
+            if ( direction === 5 || direction === 6 ) {
+
+                this.dir = ( direction === 5 ) ? 'dist' : 'antiDist';
+
+                change = ( direction === 5 ) ?  change + this.grid.lanes - 1 :
+                                                change - this.grid.lanes + 1;
             }
 
 
-            this.nextPos += change;
+            // position exists
+            if ( this.grid.fields[ this.pos + change ] ) {
+
+                this.nextPos += change;
+                this.moving = true;
+            }
+
         },
 
         animate: function() {
 
             var ctx = this.ctx,
-
                 field = this.grid.fields[ this.pos ],
-
                 step = field[this.dir][ this.counter ];
 
-            ctx.fillRect( step.x - this.size/2, step.y - this.size/2, this.size, this.size);
+            ctx.fillRect( step.x - this.size/2, step.y - this.size/2, this.size, this.size );
 
             this.counter++;
 
-            if ( this.counter === field[this.dir].length ){
+            if ( this.counter === field[this.dir].length ) {
 
                 this.pos = this.nextPos;
+                this.moving = false;
                 this.counter = 0;
             }
         },
