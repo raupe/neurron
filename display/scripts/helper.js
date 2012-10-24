412911d1f1f1c72d8af7aeff091a3874259e1da7
Autarc <Autarc@gmail.com>
2012-10-24 17:32:47 +0200

    add requestAnimationFrame polyfill

---
+++ display/scripts/helper.js
@@ -0,0 +1,37 @@
+/* requestAnimationFrame polyfill */
+
+(function() {
+
+	var lastTime = 0,
+		vendors = ['ms', 'moz', 'webkit', 'o'],
+		x;
+
+	for ( x = 0; x < vendors.length && !window.requestAnimationFrame; ++x ) {
+
+		window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+		window.cancelAnimationFrame =	window[vendors[x]+'CancelAnimationFrame'] ||
+										window[vendors[x]+'CancelRequestAnimationFrame'];
+	}
+
+	if ( !window.requestAnimationFrame ) {
+
+		window.requestAnimationFrame = function ( callback, element ) {
+
+			var currTime = new Date().getTime(),
+				timeToCall = Math.max(0, 16 - (currTime - lastTime)),
+				id = window.setTimeout(function(){callback(currTime + timeToCall);}, timeToCall);
+
+			lastTime = currTime + timeToCall;
+			return id;
+		};
+	}
+
+	if ( !window.cancelAnimationFrame ) {
+
+		window.cancelAnimationFrame = function( id ) {
+
+			clearTimeout(id);
+		};
+	}
+
+}());
