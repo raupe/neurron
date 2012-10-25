90b3acaea287163a983ced6b48c077c4394fadb9
Autarc <Autarc@gmail.com>
2012-10-25 15:31:39 +0200

    refactor configs

--- display/config.js
+++ display/config.js
@@ -10,6 +10,28 @@ var config = {
 
 		width: 320,
 		height: 240
+	},
+
+	//
+	assets: {
+
+		images: {
+
+			test: 'assets/tux-test.png',
+			julia: 'assets/tux-test.png',
+			fabian: 'assets/tux-test.png',
+			duc: 'assets/tux-test.png',
+			stefan: 'assets/tux-test.png'
+		},
+
+		sounds: {},
+		movies: {}
+	},
+
+
+	elements: {
+
+		size: 50
 	}
 };
 
