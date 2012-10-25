fcc1ab19d4fee45fc45869231b589b2873b5fa6b
Autarc <Autarc@gmail.com>
2012-10-25 11:31:07 +0200

    add polyfill .bind()

--- controller/scripts/helper.js
+++ controller/scripts/helper.js
@@ -1,16 +1,28 @@
 (function(){
 
-	if ( !Function.prototype.bind ) {
+	if (!Function.prototype.bind) {
 
-		Function.prototype.bind = function(){
+		Function.prototype.bind = function (oThis) {
 
-			var args = Array.prototype.slice.call( arguments, 1 ),
-				fn = this;
+			if (typeof this !== "function") {
 
-			return function(){
+				// closest thing possible to the ECMAScript 5 internal IsCallable function
+				throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+			}
 
-				return fn.apply( args.concat( Array.prototype.slice.call( arguments, 0) ) );
-			};
+			var aArgs = Array.prototype.slice.call(arguments, 1),
+				fToBind = this,
+				fNOP = function () {},
+				fBound = function () {
+
+				return fToBind.apply(	this instanceof fNOP && oThis ? this : oThis || window,
+										aArgs.concat(Array.prototype.slice.call(arguments)) );
+				};
+
+			fNOP.prototype = this.prototype;
+			fBound.prototype = new fNOP();
+
+			return fBound;
 		};
 	}
 
