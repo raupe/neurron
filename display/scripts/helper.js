1b643243a6c3d409fd372664392868f71477e725
Autarc <Autarc@gmail.com>
2012-10-28 13:48:27 +0100

    format

--- display/scripts/helper.js
+++ display/scripts/helper.js
@@ -8,8 +8,8 @@
 
 	for ( x = 0; x < vendors.length && !window.requestAnimationFrame; ++x ) {
 
-		window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
-		window.cancelAnimationFrame =	window[vendors[x]+'CancelAnimationFrame'] ||
+		window.requestAnimationFrame =	window[vendors[x]+'RequestAnimationFrame'];
+		window.cancelAnimationFrame  =	window[vendors[x]+'CancelAnimationFrame'] ||
 										window[vendors[x]+'CancelRequestAnimationFrame'];
 	}
 
