aad5f163f90609c5cb475e34038083ee6f6f7057
Autarc <Autarc@gmail.com>
2012-11-10 00:55:59 +0100

    transfrom debug into instance

--- display/debug.js
+++ display/debug.js
@@ -2,131 +2,121 @@
 (function(){
 
 
-	var debug = (function(){
+	var Debug = display.Debug = function(){
 
-		function debug ( screen, grid, manager, connection ) {
+		this.addPlayerInput();
 
-			addPlayerInput( manager );
+		this.drawNums();
 
-			drawNums( grid );
-
-			addVariableFrames( grid, manager );
-
-		}
-
-
-		function drawNums( grid ) {
-
-			grid.draw = function(){
-
-				this.drawCircles();
-
-				this.drawGrid();
-
-				this.drawLines();
-
-
-				this.origin.drawImage( this.ctx.canvas, this.posX, this.posY, this.width, this.height );
-			};
-		}
-
-
-		function drawLine( grid, no ) {
-
-			grid.draw = function(){
-
-				var fields = screen.grid.fields,
-
-					ctx = screen.grid.ctx;
-
-				var a = no;
-
-				fields[a].ring.forEach(function ( field ) {
-
-					ctx.fillRect( field.x-2.5,field.y-2.5,5,5);
-				});
-
-				fields[a].antiRing.forEach(function ( field) {
-					ctx.fillRect( field.x-2.5,field.y-2.5,5,5);
-				});
-
-				fields[a].dist.forEach(function ( field) {
-					ctx.fillRect( field.x-2.5,field.y-2.5,5,5);
-				});
-
-				fields[a].antiDist.forEach(function ( field) {
-					ctx.fillRect( field.x-2.5,field.y-2.5,5,5);
-				});
-			};
-
-		}
+		// this.addVariableFrames( grid, manager );
+	};
 
 
 
-		// simulate player input
-		function addPlayerInput ( manager ) {
+	Debug.prototype.drawNums = function() {
 
-			var players = manager.players,
-				key;
+		this.background.draw = function(){
 
-			document.addEventListener('keyup', function ( e ){
+			this.grid.drawCircles();
 
-				e.preventDefault();
-				e.stopPropagation();
+			this.grid.drawGrid();
 
-				key = e.which;
+			this.grid.drawLines();
 
-				if ( !players[0].moving ) {
+			this.grid.origin.drawImage( this.grid.ctx.canvas, this.grid.posX, this.grid.posY, this.grid.width, this.grid.height );
+		};
+	};
 
-					if ( key === 37 ) players[0].move( 4 );
-					if ( key === 38 ) players[0].move( 5 );
-					if ( key === 39 ) players[0].move( 3 );
-					if ( key === 40 ) players[0].move( 6 );
-				}
 
+	Debug.prototype.drawLine = function ( grid, no ) {
+
+		grid.draw = function(){
+
+			var fields = screen.grid.fields,
+
+				ctx = screen.grid.ctx;
+
+			var a = no;
+
+			fields[a].ring.forEach(function ( field ) {
+
+				ctx.fillRect( field.x-2.5,field.y-2.5,5,5);
 			});
-		}
+
+			fields[a].antiRing.forEach(function ( field) {
+				ctx.fillRect( field.x-2.5,field.y-2.5,5,5);
+			});
+
+			fields[a].dist.forEach(function ( field) {
+				ctx.fillRect( field.x-2.5,field.y-2.5,5,5);
+			});
+
+			fields[a].antiDist.forEach(function ( field) {
+				ctx.fillRect( field.x-2.5,field.y-2.5,5,5);
+			});
+		};
+
+	};
+
+
+
+	// simulate player input
+	Debug.prototype.addPlayerInput = function() {
+
+		var list, key;
+
+		document.addEventListener('keyup', function ( e ){
+
+			e.preventDefault();
+
+			e.stopPropagation();
+
+
+			playerList = this.manager.playerList;
+
+			key = e.which;
+
+			if ( !playerList[0].moving ) {
+
+				if ( key === 37 ) playerList[0].move( 4 );
+				if ( key === 38 ) playerList[0].move( 5 );
+				if ( key === 39 ) playerList[0].move( 3 );
+				if ( key === 40 ) playerList[0].move( 6 );
+			}
+
+		}.bind(this));
+	};
 
 
 		// change framestep between fields, lower framerate - *faster* animation
-		function addVariableFrames ( grid, manager ) {
+	Debug.prototype.addVariableFrames = function ( grid, manager ) {
 
-			var players = manager.players,
-				keymap = {},
-				key;
+		var players = manager.players,
+			keymap = {},
+			key;
 
-			for ( key = 48 ; key < 57; key++ ) {
+		for ( key = 48 ; key < 57; key++ ) {
 
-				keymap[key]= key - 48;
-			}
-
-			document.addEventListener('keyup', function ( e ){
-
-				e.preventDefault();
-				e.stopPropagation();
-
-				key = e.which;
-
-				if ( !players[0].moving && keymap[key] ) {
-
-					grid.frames = keymap[key] * 5; // default: 6 * 5
-					grid.definePositions.call( grid );
-
-					players[0].moving = true;
-
-					setTimeout( function(){	players[0].moving = false;	});
-				}
-			});
+			keymap[key]= key - 48;
 		}
 
+		document.addEventListener('keyup', function ( e ){
 
-		return debug;
+			e.preventDefault();
+			e.stopPropagation();
 
-	})();
+			key = e.which;
 
+			if ( !players[0].moving && keymap[key] ) {
 
+				grid.frames = keymap[key] * 5; // default: 6 * 5
+				grid.definePositions.call( grid );
 
+				players[0].moving = true;
 
-	window.debug = debug;
+				setTimeout( function(){	players[0].moving = false;	});
+			}
+		});
+	};
 
 })();
