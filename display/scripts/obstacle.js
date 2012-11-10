9545bcf7e61e9122524eed33b49dee38744012c7
Autarc <Autarc@gmail.com>
2012-11-10 12:00:36 +0100

    show obstacle "test" case

--- display/scripts/obstacle.js
+++ display/scripts/obstacle.js
@@ -11,6 +11,22 @@
 
     Obstacle.prototype = new display.Element();
 
+    Obstacle.prototype.update = function(){
 
+		if ( !this.check ) {
+
+			this.check = true;
+
+			setTimeout( function(){
+
+				this.visible = false;
+
+				this.pool.set( this.id );
+
+			}.bind(this), 1000);
+		}
+
+		//ObstaclePool.prototype.update.apply(this);
+    };
 
 })();
