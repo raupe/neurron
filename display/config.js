a68562278f04654e34c5d69efce4c8eb3d586207
Autarc <Autarc@gmail.com>
2012-11-10 14:21:05 +0100

    declare test sound

--- display/config.js
+++ display/config.js
@@ -19,16 +19,21 @@ window.config = {
 
 		images: {
 
-			player	: 'assets/alpha-test.png',
+			player		: 'assets/alpha-test.png',
 
-			damage	: 'assets/alpha-test.png',
-			heal	: 'assets/alpha-test.png',
-			points	: 'assets/alpha-test.png',
+			damage		: 'assets/alpha-test.png',
+			heal		: 'assets/alpha-test.png',
+			points		: 'assets/alpha-test.png',
 
-			bg		: 'assets/tux-test.png'
+			collision	: 'assets/alpha-test.png',
+
+			bg			: 'assets/tux-test.png'
+		},
+
+		sounds: {
+
+			collision	: 'assets/collision-test.wav'
 		}
-
-		// sounds: {},
 		// movies: {}
 	},
 
@@ -50,13 +55,16 @@ window.config = {
 	obstacles: {
 
 		// type defines the image key
-		1: { type: 'damage'	, size: 1*40, velocity: 0.5, value:  10, color: [ 213,  10,  50 ] }, // red
+		1: { type: 'damage'	, size: 1*40, velocity: 0.5, value:  10, color: [ 213,  10,  50 ],
+				collisionImg: 'collision', collisionSound: 'collision' }, // red
 
-		2: { type: 'heal'	, size: 1*40, velocity: 1  , value:  10, color: [   0, 170,  30 ] }, // grean
+		2: { type: 'heal'	, size: 1*40, velocity: 1  , value:  10, color: [   0, 170,  30 ],
+				collisionImg: 'collision', collisionSound: 'collision' }, // grean
 
-		3: { type: 'points'	, size: 1*40, velocity: 1  , value: 100, color: [ 240, 220,  10 ] }, // yellow
+		3: { type: 'points'	, size: 1*40, velocity: 1  , value: 100, color: [ 240, 220,  10 ],
+				collisionImg: 'collision', collisionSound: 'collision' }, // yellow
 
-		4: { type: 'points'	, size: 1*40, velocity: 1  , value:  50, color: [ 240, 220,  10 ] }
-
+		4: { type: 'points'	, size: 1*40, velocity: 1  , value:  50, color: [ 240, 220,  10 ],
+				collisionImg: 'collision', collisionSound: 'collision' }
 	}
 };
