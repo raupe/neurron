d8f6c6aff477bd86852012b9843ff0e7a426d602
Autarc <Autarc@gmail.com>
2012-11-14 00:05:08 +0100

    show placeholder

--- display/scripts/statusmanager.js
+++ display/scripts/statusmanager.js
@@ -7,7 +7,40 @@
 	};
 
 
-	StatusManager.prototype.init = function ( playerlist )  {
+	StatusManager.prototype.init = function ( playerList )  {
+
+		this.playerList = playerList;
+
+		this.updatePoints( 0 );
+		this.updateLifeBars( 100 );
+	};
+
+
+	StatusManager.prototype.updatePoints = function ( value ) {
+
+		var ctx = this.panel,
+			size = 40;
+
+		ctx.fillStyle = 'red';
+		ctx.font = 'italic ' + size + 'pt Arial';
+		ctx.fillText( value + ' points', this.offset/5, size * 2 );
+	};
+
+
+	StatusManager.prototype.updateLifeBars = function ( energy ) {
+
+		var ctx = this.panel,
+			playerList = this.playerList;
+
+		for ( var i = 0, l = playerList.length; i < l; i++ ){
+
+			ctx.fillStyle = 'green';
+			ctx.fillRect( 20, 200, 300, 40);
+
+			ctx.fillStyle = 'white';
+			ctx.font = 'italic ' + 20 + 'pt Arial';
+			ctx.fillText( energy + ' %', this.offset/2.5, 230 );
+		}
 
 	};
 
@@ -29,13 +62,13 @@
 
 		ctx.fillRect( 0, 0, cvs.width, cvs.height );
 
-		this.panel = cvs;
+		this.panel = ctx;
 	};
 
 
 	StatusManager.prototype.draw = function(){
 
-		this.screen.ctx.drawImage( this.panel, this.start, 0 );
+		this.screen.ctx.drawImage( this.panel.canvas, this.start, 0 );
 	};
 
 
