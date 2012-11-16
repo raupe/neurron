7dcb22b7376379995bdac299fc13b37dc0da1a89
Duc Ngo Viet <d.ngoviet@gmail.com>
2012-11-16 16:21:25 +0100

    remove handle collide call from manager

--- display/scripts/manager.js
+++ display/scripts/manager.js
@@ -122,7 +122,7 @@
         this.init(2);
 		this.playerList	= new display.PlayerList( params[0] );
 		this.statusManager.init( this.playerList );
-        this.statusManager.handleCollide(1, [2]);
+        
         new display.Debug();
 	};
 
