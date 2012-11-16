dd77c48a16e3ad27877ff793602e185ebb74d5b2
Duc Ngo Viet <d.ngoviet@gmail.com>
2012-11-16 13:47:05 +0100

    adjust color input, now rgb

--- display/scripts/connection.js
+++ display/scripts/connection.js
@@ -24,8 +24,9 @@
 			console.log('[close]');
 
 			 manager.handle( config.protocol.START, [[
-                 { id:1, pos:1, color:'55' },
-                 { id:2, pos:2, color:'15' }
+                 { id:1, pos:1, color:{r: 255, g: 0, b: 0} },
+                 { id:2, pos:2, color:{r: 0, g: 255, b: 0} },
+                 { id:3, pos:4, color:{r: 0, g: 0, b: 255} }
              ]] );
 
             // manager.init(2);
