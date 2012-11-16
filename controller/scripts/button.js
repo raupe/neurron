49faf19873e7988631c1b12c08107e2d929dff00
lingfa <fabian.ling@gmx.de>
2012-11-16 15:18:58 +0100

    make button and label

--- controller/scripts/button.js
+++ controller/scripts/button.js
@@ -1,8 +1,13 @@
 (function(){
 
-	var Button = controller.Button =  function ( text ) {
+	var Button = controller.Button =  function ( type, text ) {
 
+        this.type = type;
 		this.text = text;
+        this.width = 200;
+        this.height = 100;
+        this.left = 0;
+        this.top = 0;
 
 		this.init();
 	};
@@ -12,17 +17,59 @@
 	Button.prototype.init = function(){
 
 		var box = document.createElement('div');
-
 		box.id = 'start';
-		box.className = 'button';
 
-		box.innerHTML = this.text;
+        if( this.type === 'button'){
+
+		    box.className = 'button';
+        }else{
+
+            box.className = 'label';
+        }
+
+        var textBox = document.createElement('div');
+        textBox.className = 'textBox';
+        textBox.innerHTML = this.text;
+
+        box.appendChild( textBox );
+        this.scale();
+
+        this.pos();
+
+        box.setAttribute(
+            'style',
+            'width: '+this.width+'px; ' +
+            'height: '+this.height+'px; ' +
+            'left:'+this.left+'px; ' +
+            'top:'+this.top+'px;'
+        );
 
 		box.addEventListener('touchend', this.onclick.bind(this) );
 
 		document.body.appendChild( box );
 	};
 
+    window.addEventListener("orientationchange", function() {
+
+        box.parentNode.removeChild( box );
+        this.init();
+
+    }.bind(this), false);
+
+    Button.prototype.scale = function(){
+
+        this.width = Math.round(window.innerWidth/2);
+
+        this.height = Math.round(window.innerHeight/2);
+
+    }
+
+    Button.prototype.pos = function(){
+
+        this.left = Math.round((window.innerWidth/2) - (this.width/2));
+
+        this.top = Math.round(window.innerHeight/2) - (this.height/2);
+    };
 
 	Button.prototype.onclick = function(){
 
