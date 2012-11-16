93daf47e0e7a6b941fd9e82b3c1e83a0d0f9fd2f
Duc Ngo Viet <d.ngoviet@gmail.com>
2012-11-16 11:38:47 +0100

    basic display of lifebars, labels and colorbars

--- display/scripts/statusmanager.js
+++ display/scripts/statusmanager.js
@@ -11,6 +11,13 @@
 
 		this.playerList = playerList;
 
+        this.fullBar = this.offset / 1.6;
+        this.lifeBarStartX = this.offset / 8;
+        this.colorBarStartX = this.lifeBarStartX / 2;
+        this.lifeLabelStartX = this.lifeBarStartX + this.fullBar;
+        this.startY = 140;
+        this.color = 'green';
+
 		this.updatePoints( 0 );
 		this.updateLifeBars( 100 );
 	};
@@ -21,25 +28,54 @@
 		var ctx = this.panel,
 			size = 40;
 
-		ctx.fillStyle = 'red';
+		ctx.fillStyle = 'yellow';
 		ctx.font = 'italic ' + size + 'pt Arial';
 		ctx.fillText( value + ' points', this.offset/5, size * 2 );
 	};
 
-
+    // TODO set limits in config for red, orange/yellow, green
 	StatusManager.prototype.updateLifeBars = function ( energy ) {
 
 		var ctx = this.panel,
-			playerList = this.playerList;
+			playerList = this.playerList,
+            currentPlayer,
+            r,
+            g,
+            b;
 
 		for ( var i = 0, l = playerList.length; i < l; i++ ){
 
-			ctx.fillStyle = 'green';
-			ctx.fillRect( 20, 200, 300, 40);
+            currentPlayer = playerList[i];
+            currentPlayer.energy = 100;
+            r = currentPlayer.color[0];
+            g = currentPlayer.color[1];
+            b = currentPlayer.color[2];
 
+            if (currentPlayer.energy <= config.colorLimits.red) {
+
+                this.color = 'red';
+
+            } else if ( currentPlayer.energy <= config.colorLimits.orange ) {
+
+                this.color = 'orange';
+
+            } else {
+
+                this.color = 'green';
+            }
+
+            // lifeBars
+			ctx.fillStyle = this.color;
+			ctx.fillRect( this.lifeBarStartX, this.startY + i*50, this.fullBar * (currentPlayer.energy / 100), 40);
+
+            // lifeLabels
 			ctx.fillStyle = 'white';
 			ctx.font = 'italic ' + 20 + 'pt Arial';
-			ctx.fillText( energy + ' %', this.offset/2.5, 230 );
+			ctx.fillText( currentPlayer.energy + ' %', this.lifeLabelStartX, this.startY + 30 );
+
+            // colorBar
+            ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+            ctx.fillRect(this.colorBarStartX, this.startY + i*50, this.lifeBarStartX/2, 40);
 		}
 
 	};
@@ -50,7 +86,7 @@
 		var cvs = document.createElement('canvas'),
 			ctx = cvs.getContext('2d');
 
-			this.offset = this.screen.cvs.width / factor;
+		this.offset = this.screen.cvs.width / factor;
 
 		cvs.width = this.offset;
 		cvs.height = this.screen.cvs.height;
