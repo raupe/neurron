888c111fb30d39c25ea45aa70222fd4a9ded7614
Autarc <Autarc@gmail.com>
2012-11-17 17:33:06 +0100

    undo changes

--- display/main.js
+++ display/main.js
@@ -1,69 +1,22 @@
 (function(){
 
+     // loading assets
+    var manager = new display.AssetManager( config.assets );
 
-    new display.Debug();
+    manager.on('progress', function ( e ) {
 
-
-
-    // available scenes - default / current scene - fetch sites , parse , then callback what to do
-    new display.Scenemanager({
-
-        start : 'views/start',
-        load  : 'views/load',
-        game  : 'views/game',
-        end   : 'views/end'
-
-    }, function ( scene ) { // reference to this - as a single scene !
-
-        scene.show('start');
-
-
-        // console.log(scene);
-        // document.addEventListener('click', function(){
-
-        //     scene.show('load');
-        //     //scene.changeTo('load');//, function() { // show + transition , optional paramter as a callback
-
-        // });
-
-
-
-        // // loading assets
-        // var manager = new display.AssetManager( config.assets );
-
-
-        // manager.on('progress', function ( e ) {
-
-        //     // console.log(e.progress);
-        // });
-
-
-
-
-        // manager.on('load', function(){
-
-        //     // console.log(manager.assets);
-
-        //     new display.Screen();
-
-        //     new display.Manager();
-
-        //     new display.Connection();
-        // });
-
-
-        // });
-
-        // scenemanager.on('load', function(){  });
-
-        // scenemanager.on('game', function(){});
-
-
+        // console.log(e.progress);
     });
 
+    manager.on('load', function(){
 
+        // console.log(manager.assets);
 
+        new display.Screen();
 
+        new display.Manager();
 
+        new display.Connection();
+    });
 
 })();
