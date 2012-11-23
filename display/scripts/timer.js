fb6bc293d60de071977981091a57db3f04c01263
Duc Ngo Viet <d.ngoviet@gmail.com>
2012-11-23 10:52:22 +0100

    finish timer.js

--- display/scripts/timer.js
+++ display/scripts/timer.js
@@ -4,20 +4,51 @@
 
         this.timeLeft = milliseconds;
 
+        this.timeLeftString = "";
+
         this.loop();
     }
 
     Timer.prototype.loop = function() {
-        this.timeLeft-=1000;
+
+        this.timeLeft-=1000; // subtract 1 second
+
+        this.setTimeLeftString();
+        this.displayTimeLeft();
+
+        if (this.timeLeft >= 1000 ) { // just call loop if timeLeft is greater than 1 second
+
+            window.setTimeout(this.loop.bind(this), 1000);
+        }
+    }
+
+    Timer.prototype.setTimeLeftString = function() {
 
         var min = ~~((this.timeLeft/1000) / 60);
         var seconds = ~~(this.timeLeft/1000) - (min * 60);
-        console.log(min+":"+seconds);
 
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
 
-        if (this.timeLeft >= 1000 ) {
+        this.timeLeftString = "0" + min + ":" + seconds;
+    }
 
-            window.setTimeout(this.loop.bind(this), 1000);
+    Timer.prototype.displayTimeLeft = function() {
+
+        var timerContainer = document.getElementById("timer");
+
+        if (!timerContainer) {
+
+            timerContainer = document.createElement('div');
+            timerContainer.setAttribute("id", "timer");
+            timerContainer.setAttribute("class", "timer");
+            timerContainer.innerHTML = this.timeLeftString;
+
+            document.body.appendChild( timerContainer );
+        } else {
+
+            timerContainer.innerHTML = this.timeLeftString;
         }
     }
 })();
