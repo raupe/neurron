b0f4bfe54db0aed3a6be03341df020175c92db87
Duc Ngo Viet <d.ngoviet@gmail.com>
2012-11-23 11:11:50 +0100

    call countdown

--- display/scripts/manager.js
+++ display/scripts/manager.js
@@ -117,7 +117,7 @@
 
 	Manager.prototype.countdown = function() {
 
-		console.log('countdown...');
+        new display.Timer( 5 * 1000, "countdown");
 	};
 
 
@@ -144,7 +144,7 @@
 
 		new display.Debug();
 
-        new display.Timer( 3 * 60 * 1000);
+        new display.Timer( 3 * 60 * 1000, "timer");
 	};
 
 
