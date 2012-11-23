134178cedbd74103b46b7b1cb3c21a158ae7b1b4
Autarc <Autarc@gmail.com>
2012-11-23 11:43:43 +0100

    refactor label & commands

--- controller/config.js
+++ controller/config.js
@@ -4,16 +4,31 @@ window.controller = {};
 /* configuraitons */
 var config = {
 
-	server		: 'neurron.com',
-	port		: '2020',
+	server			: 'neurron.com',
+	port			: '2020',
 
 	// basic resolution
 	canvas: {
 
-		width: 320,
-		height: 240
+		width		: 320,
+		height		: 240
 	},
 
+	buttons: {
+
+		1			: ['label', 'The game has ended'],
+		2			: ['label', 'The game is already running'],
+		3			: ['label', 'Game not found']
+	},
+
+
+
+	commands:	{
+
+		REGISTER	: 1,
+		MOVE		: 2,
+		HEAL		: 3
+	},
 
 
 	protocol: {
