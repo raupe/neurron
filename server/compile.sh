1509247ae895fbe6fc3a7c64fd5f0b253900f4a2
Julia <Julia@Cleo.(none)>
2012-11-29 11:59:32 +0100

    stop server before compiling and restart

--- server/compile.sh
+++ server/compile.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+# stop server
+svc -hd ~/service/gameserver/
 
+# adapt paths
 sed -i 's/C:\/Program Files (x86)\/boost\//\/home\/neurron\/neurron\//g' Server/Server.makefile
 sed -i 's/stage\/gcclib\"/stage\/lib\" -lboost_thread -lboost_chrono -lboost_system/g' Server/Server.makefile
 
-
+# compile
 make
+
+# start server
+svc -hu ~/service/gameserver/
