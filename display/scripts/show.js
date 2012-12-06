81a1fc9bb0c5a08504d03c06a33520c68a8e0b1a
Autarc <Autarc@gmail.com>
2012-12-06 16:55:02 +0100

    show content

--- display/scripts/show.js
+++ display/scripts/show.js
@@ -1,15 +1,56 @@
 (function(){
 
-	var left = document.getElementById('container-left'),
-		right = document.getElementById('container-right');
+	var containerLeft = document.getElementById('container-left'),
+		containerRight = document.getElementById('container-right');
+
+    display.current = null;
 
 
-	display.show = function ( view ) {
+	display.show = function ( id ) {
 
-		view = display.views[view];
+        if ( display.current ) {
 
-		left.innerHTML	= view[0];
-		right.innerHTML	= view[1];
+            document.getElementById( display.current + '-l' ).className = 'hide';
+            document.getElementById( display.current + '-r' ).className = 'hide';
+        }
+
+        display.current = id;
+
+        var left = document.getElementById( id + '-l' ),
+            right = document.getElementById( id + '-r' ),
+
+            view = display.views[ id ],
+            temp;
+
+
+        if ( left ) {
+
+            left.className = 'show';
+
+        } else if ( view.left ) {
+
+            temp = document.createElement('div');
+            temp.innerHTML = view.left;
+            temp.id = id + '-l';
+            temp.className = 'wrapper';
+            containerLeft.appendChild( temp );
+        }
+
+
+        if ( right ) {
+
+            right.className = 'show';
+
+        } else if ( view.right ) {
+
+            temp = document.createElement('div');
+            temp.innerHTML = view.right;
+            temp.id = id + '-r';
+            temp.className = 'wrapper';
+            containerRight.appendChild( temp );
+        }
+
+        display.logic[ id ]();
 	};
 
 })();
