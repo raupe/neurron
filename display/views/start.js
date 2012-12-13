4c5e39bd7a7fe3a87147df705d2e1ed145cf3768
lingfa <fabian.ling@gmx.de>
2012-12-13 15:45:04 +0100

    make button navigation

--- display/views/start.js
+++ display/views/start.js
@@ -6,29 +6,47 @@
 $(document).ready(function(){
 
     var duration = {
-        intro : 5000,
-        how2play : 8000,
-        demo : 9000
+        intro : 4000,
+        how2play : 4000,
+        demo : 4000
     };
 
     var $items = $(".screen_wrap li"),
+
+        $buttons = $(".slider_box_wrap li"),
+
+        timer = 0;
 
         counter = 0,
 
         itemsLength = $items.length;
 
+    $($buttons).click(function(){
 
+        $($buttons[counter]).removeClass('button_active');
+        $($items[counter]).fadeOut();
+        counter = $(this).index();
+        clearTimeout(timer);
+        timeOut();
+
+    });
     //set time out
     var timeOut = function(){
 
         var currentId = $items[counter].id,
 
             durationTime = duration[currentId];
-        $($items[counter]).fadeIn();
-        setTimeout(function() {
 
-           console.log(currentId +'  '+ durationTime);
+        $($items[counter]).fadeIn();
+        $($buttons[counter]).addClass('button_active');
+        //$buttons[counter].className += " button_active";
+
+        timer = setTimeout(function() {
+
+           //console.log(currentId +'  '+ durationTime);
             $($items[counter]).fadeOut();
+            $($buttons[counter]).removeClass('button_active');
+            //$buttons[counter].className += " button_active";
             counter++;
 
            if( (counter) >= itemsLength) counter = 0;
@@ -39,5 +57,7 @@ $(document).ready(function(){
     }
 
     timeOut();
+
+
 
 });
