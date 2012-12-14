55187b8cb3082df7f27a1282c9f13945e70c2211
Duc Ngo Viet <d.ngoviet@gmail.com>
2012-12-14 15:11:24 +0100

    move clear from end to start so you can see what you've drawn

--- controller/scripts/input.js
+++ controller/scripts/input.js
@@ -122,6 +122,8 @@
 		e.preventDefault();
 		e.stopPropagation();
 
+        this.screen.clear();
+
 		this.tapped = true;
 
 
@@ -195,7 +197,6 @@
 
 		manager.handle( config.commands.MOVE, [ origin, touch, this.averageX, this.averageY, this.between ] );
 
-		this.screen.clear();
 
         this.between.length = 0;
         this.averageX = 0;
