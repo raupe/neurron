d0e73f8d9e681a4b577fd97eef359171b4912c72
Duc Ngo Viet <d.ngoviet@gmail.com>
2012-12-14 12:03:33 +0100

    add load js

--- display/views/view_load.js
+++ display/views/view_load.js
@@ -1,12 +1,14 @@
 (function(){
-/*
+
 	display.views.load = (function(){
 
-		var left = '<img src="assets/views/load/load_background.jpg"/>',
+		var left = '<img class="load_background" src="assets/views/load/load_background.jpg"/>\
+                <div id="load_countdown_timer"></div>',
 
 
 
-            right = 'right';
+            right = '<div id="load_register_status">\
+                </div>';
 
 
         return {
@@ -16,23 +18,26 @@
         };
 
 	})();
-*/
+
     display.load_view.playerNums = 0;
 
     display.load_view.showNewPlayer = function(){
         console.log("new player added");
         display.load_view.playerNums++;
 
-        var element = document.getElementById("load_register_status");
-
+        var element = document.getElementById("load_register_status"),
+            image = document.createElement("img"),
+            pathToImages = "http://localhost/projects/neurron/display/assets/tmp/";
+            image.src = pathToImages + "ronron" +display.load_view.playerNums+ ".png";
+            element.appendChild(image);
     };
 
-    display.load_view.showNewPlayer();
-    console.log(display.load_view.playerNums);
+    display.load_view.clearPlayerNums = function(){
+        display.load_view.playerNums = 0;
+    };
 
     display.logic.load = function(){
 
-//        display.show( 'game' );
-    };
+    }
 
 })();
