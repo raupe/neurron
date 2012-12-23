c169a85101c1d094e63684298c9e2e309b9c30ac
Autarc <Autarc@gmail.com>
2012-12-23 23:42:52 +0100

    insert fading background arcs

--- controller/scripts/screen.js
+++ controller/scripts/screen.js
@@ -23,12 +23,77 @@
 		window.addEventListener('orientationchange', this.scale.bind( this ) );
 	};
 
+
 	Screen.prototype.scale = function() {
 
 		this.cvs.width = window.innerWidth;
 		this.cvs.height = window.innerHeight;
 
         if ( this.input ) this.input.setStyle();
+	};
+
+
+	Screen.prototype.setBackground = function ( colorId ) {
+
+		var cvs = this.cvs;
+
+		if ( colorId ) {
+
+			var bvs = document.createElement('canvas'),
+				btx = bvs.getContext('2d'),
+				color = config.backgroundColors[ colorId ],
+
+				width	= 10,
+				opacity = 0.7,
+				height	= width * 20,
+				steps	= 60,
+				value	= 255/steps,
+				time	= 200;// 500;
+
+			bvs.width = this.cvs.width;
+			bvs.height = this.cvs.height;
+
+			var	cx = bvs.width/2,
+				cy = bvs.height/2,
+				rad = ( Math.min( cx, cy )/10 ) * 9,
+				grd = btx.createRadialGradient( cx, cy, rad - 4 * width, cx, cy, rad );
+
+			grd.addColorStop( 0, '#000' );
+			grd.addColorStop( 0.3, 'rgba(' + color.r + ',' + color.g + ',' + color.b + ',' + opacity + ')' );
+			grd.addColorStop( 0.6, 'rgba(' + color.r + ',' + color.g + ',' + color.b + ',' + opacity + ')' );
+			grd.addColorStop( 1, '#000');
+
+			btx.fillStyle = grd;
+			btx.fillRect( 0, 0, bvs.width, bvs.height );
+
+			// center "line"
+			btx.fillStyle = '#000';
+			btx.fillRect( 0, cy - height/2, bvs.width, height);
+
+			setTimeout( fade, time );
+
+		} else {
+
+			cvs.style['background'] = '#000';
+		}
+
+
+		function fade() {
+
+			var img = btx.getImageData( 0, 0, bvs.width, bvs.height ),
+
+				length = img.width * img.height,
+
+				pixels = img.data;
+
+			do { pixels[ (length << 2)+3 ] = value * steps;	} while ( length-- );
+
+			btx.putImageData( img, 0, 0 );
+
+			cvs.style['background'] = 'url(' + bvs.toDataURL('image/png') + ') no-repeat #000';
+
+			if ( steps-- ) setTimeout( fade, time );
+		}
 	};
 
 
@@ -44,3 +109,7 @@
 	};
 
 })();
+
+
+
+
