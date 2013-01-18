83df4a893018dcb0443e82dde52a6662e7b7253e
Autarc <Autarc@gmail.com>
2013-01-18 14:10:20 +0100

    fix input

--- controller/scripts/input.js
+++ controller/scripts/input.js
@@ -15,67 +15,84 @@
 			averageY = 0,
 			counter = 0,
 
-			color = null,
+			shadowColor = null,
 			enabled = false,
 
 			clearing = null,
 			timer = null,
 
-			paused = null,
 			//tapped = false,
+			paused = null,
+			manager = null;
 
-			manage; // temp
-
-
-		// ---- public ----- //
-
-		var active = function ( change ) {
-
-				enabled = change;
-			},
-
-
-			setStyle = function ( param ) {
-
-				if ( param ) color = config.shadowColors[ config.colors[ param ] ];
-
-				ctx.lineWidth = 10;
-				ctx.lineCap = 'round';
-				ctx.lineJoin = 'round';
-
-				ctx.strokeStyle = '#fff';
-
-				ctx.shadowBlur = 20;
-				ctx.shadowColor = color ? 'rgb(' + color.r + ',' + color.g + ',' + color.b + ')' : '#BAE9F7';
-			},
-
-
-			init = function ( handle ) {
-
-				// setup
-				if ( 'ontouchstart' in window ) {
-
-					handleTouch();
-
-					setStyle();
-				}
-
-				manage = handle;
-
-				return this;
-			};
 
 
 		// ----- private ----- //
 
-		function handleTouch() {
+		function draw(){
 
-			cvs.addEventListener('touchstart', start );
-			cvs.addEventListener('touchmove', move );
-			cvs.addEventListener('touchend', end );
-			cvs.addEventListener('touchcancel', cancel );
-			cvs.addEventListener('touchleave', leave );
+			var l = points.length - 2,
+
+				x2, y2;	// temp
+
+			// reset start
+			ctx.beginPath();
+
+			if ( !enabled || l <= 0 ) return;
+
+			ctx.moveTo( points[0].x, points[0].y );
+
+			for ( var i = 1; i < l; i++ ) {
+
+				x2 = ( points[i].x + points[i+1].x ) / 2;
+				y2 = ( points[i].y + points[i+1].y ) / 2;
+
+				ctx.quadraticCurveTo( points[i].x, points[i].y, x2, y2 );
+			}
+
+			ctx.quadraticCurveTo( points[i].x, points[i].y,	points[i+1].x, points[i+1].y );
+
+			ctx.stroke();
 		}
+
+		function clear () {
+
+			timer = null;
+
+			if ( clearing ) return;
+
+			paused = false;
+
+			clearing = setInterval(function(){
+
+				if ( clearing && !points.length ) {
+
+					clearInterval( clearing );
+					clearing = null;
+
+					paused = true;
+					return;
+				}
+
+				points = points.slice(1);
+
+				ctx.clearRect( 0, 0, cvs.width, cvs.height );
+
+				draw();
+
+			}, config.clearRate );
+		}
+
+
+		function getPos ( e ) {
+
+			return {
+
+				x: e.clientX || e.offsetX || e.layerX || e.pageX,
+				y: e.clientY || e.offsetY || e.layerY || e.pageY
+			};
+		}
+
 
 		function start ( e ) {
 
@@ -89,6 +106,7 @@
 			ctx.clearRect( 0, 0, cvs.width, cvs.height );
 			points.length = 0;
 			clearing = null;
+
 
 			var touch = getPos( e.changedTouches[0] );
 			// tapped = true;
@@ -120,7 +138,6 @@
 			averageY += touch.y;
 			counter++;
 
-			// line
 			if ( paused ) timer = setTimeout( clear, config.clearDelay );
 
 			draw();
@@ -140,7 +157,7 @@
 				// heal
 				if ( tapped ) {
 
-					manage( config.commands.HEAL );
+					manager.heal( config.commands.HEAL );
 					return;
 				}
 
@@ -150,7 +167,7 @@
 			averageX /= counter;
 			averageY /= counter;
 
-			manage( config.commands.MOVE,[ origin, touch, averageX, averageY, between ] );
+			manager.move([ origin, touch, averageX, averageY, between ]);
 
 			between.length = 0;
 			averageX = 0;
@@ -158,74 +175,6 @@
 			counter = 0;
 		}
 
-
-		function clear() {
-
-			timer = null;
-
-			if ( clearing ) return;
-
-			paused = false;
-
-			clearing = setInterval(function(){
-
-				if ( !points.length ) {
-
-					clearInterval( clearing );
-					clearing = null;
-
-					paused = true;
-					return;
-				}
-
-				points = points.slice(1);
-
-				ctx.clearRect( 0, 0, cvs.width, cvs.height );
-
-				// -> remove instant
-				// points.length = 0;
-
-				draw();
-
-			}, config.clearRate );
-		}
-
-
-		function draw(){
-
-			var l = points.length - 2,
-
-				x2, y2;	// temp
-
-			// reset start
-			ctx.beginPath();
-
-			if ( !enabled || l <= 0 ) return;
-
-			ctx.moveTo( points[0].x, points[0].y );
-
-			for ( var i = 1; i < l; i++ ) {
-
-				x2 = ( points[i].x + points[i+1].x ) / 2;
-				y2 = ( points[i].y + points[i+1].y ) / 2;
-
-				ctx.quadraticCurveTo( points[i].x, points[i].y, x2, y2 );
-			}
-
-			ctx.quadraticCurveTo( points[i].x, points[i].y,	points[i+1].x, points[i+1].y );
-
-			ctx.stroke();
-		}
-
-
-		function getPos ( e ) {
-
-			return {
-
-				x: e.clientX || e.offsetX || e.layerX || e.pageX,
-				y: e.clientY || e.offsetY || e.layerY || e.pageY
-			};
-		}
 
 		function cancel ( e ) {
 
@@ -241,15 +190,68 @@
 		}
 
 
-		// pass for resize
-		screen.init( setStyle );
+		function handleTouch() {
 
-		return {
+			cvs.addEventListener('touchstart', start );
+			cvs.addEventListener('touchmove', move );
+			cvs.addEventListener('touchend', end );
+			cvs.addEventListener('touchcancel', cancel );
+			cvs.addEventListener('touchleave', leave );
+		}
+
+
+		function style() {
+
+			ctx.lineWidth = 10;
+			ctx.lineCap = 'round';
+			ctx.lineJoin = 'round';
+
+			ctx.strokeStyle = '#fff';
+
+			ctx.shadowBlur = 20;
+			ctx.shadowColor = shadowColor ? 'rgb(' + shadowColor.r + ',' + shadowColor.g + ',' + shadowColor.b + ')' : '#BAE9F7';
+		}
+
+
+		// ---- public ----- //
+
+		var init = function ( ref ) {
+
+			manager = ref;
+
+			if ( 'ontouchstart' in window ) handleTouch();
+
+			screen.init( style );
+		};
+
+		var setStyle = function ( param ) {
+
+			var color = config.colors[ param ];
+
+			shadowColor = config.shadowColors[ color ];
+
+			screen.cvs.className = 'background ' + color;
+
+			style();
+		};
+
+		var active = function ( change ) {
+
+			enabled = change;
+
+			if ( !change ) points.length = 0; // clear
+		};
+
+
+		// Interface
+		var Input = {
 
 			init	: init,
-			active	: active,
-			setStyle: setStyle
+			setStyle: setStyle,
+			active	: active
 		};
+
+		return Input;
 
 	})();
 
